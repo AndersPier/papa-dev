e6c97aaa1671f74b270b1a4686f46927d430e4ff
Anders Lehmann Pier <anders@pier.dk>
2024-02-11 23:52:58 +0100

    bootstrap commit

---
+++ bootstrap/run.sh
@@ -0,0 +1,5 @@
+  GNU nano 6.2                              gittest.sh                                        
+#!/bin/bash
+echo -e "Hello there, this is the very first bootstrap run !!!"
+ls -ltrah
+df -h

d2aae75cf2f6841d22aa0447de89d9bd2736c0dc
Anders Lehmann Pier <anders@pier.dk>
2024-02-12 00:59:32 +0100

    Update run.sh

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -1,4 +1,3 @@
-  GNU nano 6.2                              gittest.sh                                        
 #!/bin/bash
 echo -e "Hello there, this is the very first bootstrap run !!!"
 ls -ltrah

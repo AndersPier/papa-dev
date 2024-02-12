49a221e550d5c7072e54860275229256b612c70e
Anders Lehmann Pier <anders@pier.dk>
2024-02-12 01:42:34 +0100

    Update run.sh

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -2,5 +2,5 @@
 echo -e "Hello there, this is the very first bootstrap run !!!"
 #ls -ltrah
 df -h
-echo -e "${USER} was here" > ~/$USER-was-here
+echo -e "${USER} was here" > ~/kilroy-was-here
 sudo ls -ltrah

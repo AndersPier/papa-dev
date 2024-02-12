e94349d169479569b90d1525c1f570d267257b3b
Anders Lehmann Pier <anders@pier.dk>
2024-02-12 15:33:23 +0100

    Update run.sh

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo -e "Hello there, this is the very first bootstrap run !!!"
-#ls -ltrah
+echo -e "Free space:"
 df -h
-#echo -e "${USER} was here" > ~/kilroy-was-here
-#sudo ls -ltrah
-docker ps
-docker volume ls
+echo -e "Running compose stacks:"
+docker compose ps
